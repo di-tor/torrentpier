0eee81089049933aec6e1af2c46fb086ef97ac3d
Vasily Komrakov <diolektor@gmail.com>
2018-03-31 19:14:53 +0300

    Added exceptions for the configuration library. - check if a file exists - check if the file is readable

---
+++ src/Configure/Exception/FileNotExistException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * TorrentPier – Bull-powered BitTorrent tracker engine
+ *
+ * @copyright Copyright (c) 2005-2018 TorrentPier (https://torrentpier.com)
+ * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
+ * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
+ */
+
+namespace TorrentPier\Configure\Exception;
+
+class FileNotExistException extends \Exception
+{
+    protected $message = 'File not exist';
+}
--- src/Configure/Reader/ArrayFileReader.php
+++ src/Configure/Reader/ArrayFileReader.php
@@ -9,20 +9,32 @@
 
 namespace TorrentPier\Configure\Reader;
 
+use TorrentPier\Configure\Exception\FileNotExistException;
+use TorrentPier\Configure\Exception\ReadUnavailableException;
+
 class ArrayFileReader extends ArrayReader
 {
     /**
      * ArrayFileConfig constructor.
      * @param $pathToFile
      * @param string $name
+     * @throws FileNotExistException
+     * @throws ReadUnavailableException
      */
     public function __construct($pathToFile, $name = 'config')
     {
         ${$name} = [];
 
+        if (!file_exists($pathToFile)) {
+            throw new FileNotExistException();
+        }
+
+        if (!is_readable($pathToFile)) {
+            throw new ReadUnavailableException();
+        }
+
         require $pathToFile;
 
         parent::__construct(${$name});
-
     }
 }
